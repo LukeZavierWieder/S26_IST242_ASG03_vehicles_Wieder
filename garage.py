a9162d73bf068f088d9574089a4ef5aa337ddba4
LukeZavierWieder <lzw5614@psu.edu>
2026-03-04 15:21:55 -0500

    Fix: changed garage to pass test cases

--- garage.py
+++ garage.py
@@ -20,32 +20,32 @@ class Garage:
         '''
         Add a vehicle to the garage
         '''
-        self._vehicle.append(vehicle)
+        self._vehicles.append(vehicle)
     
-    def clear_garage(self):
+    def empty_garage(self):
         '''
         Empties the garage of vehicles
         '''
-        self._vehicle.clear()
+        self._vehicles.clear()
 
     def remove_vehicle(self, vehicle: Vehicle):
         '''
         Takes vehicle out of garage
         '''
-        self._vehicle.pop(vehicle)
+        self._vehicles.pop(vehicle)
 
     @property
     def vehicles(self):
         '''
         Returns a copy of the internal list of vehicles
         '''
-        return list[self._vehicles]
+        return list(self._vehicles)
     
     def sort_by_release_year(self):
         '''
         Sorts garage based on release year
         '''
-        self._vehicle.sort()
+        self._vehicles.sort()
     
     # Printing
     def __str__(self):
